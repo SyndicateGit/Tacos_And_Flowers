b224793490e063004e1edd13de841a089a425178
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-02 23:20:34 -0500

    Changed model and controller to be more generic

--- models/item.js
+++ models/item.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const tacoSchema = new Schema({
+const itemSchema = new Schema({
   name: {type: String, required: true, minLength: 2},
   // cloudinary_id: {type: String},
   price: {type: Schema.Types.Decimal128, required: true},
   description: {type: String, required: true, maxLength: 300},
 });
 
-tacoSchema.virtual("url").get(function(){
+itemSchema.virtual("url").get(function(){
   return `/inventory/items/${this._id}`;
 });
 
-module.exports = mongoose.model("taco", tacoSchema);
+module.exports = mongoose.model("item", itemSchema);
