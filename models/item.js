042e657adb2f9484761858865d6bcdf4013c1541
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-02 23:21:45 -0500

    Changed itemSchema to include category attribute

--- models/item.js
+++ models/item.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
   name: {type: String, required: true, minLength: 2},
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   // cloudinary_id: {type: String},
   price: {type: Schema.Types.Decimal128, required: true},
   description: {type: String, required: true, maxLength: 300},
@@ -13,4 +14,4 @@ itemSchema.virtual("url").get(function(){
   return `/inventory/items/${this._id}`;
 });
 
-module.exports = mongoose.model("item", itemSchema);
+module.exports = mongoose.model("Item", itemSchema);
