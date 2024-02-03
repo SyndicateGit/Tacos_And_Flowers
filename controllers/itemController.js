f6c1aec8fe6c58d698f455fc74dc4f027d1ad352
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-03 11:44:28 -0500

    Added item list to items page

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,5 @@
 const Item = require("../models/item");
+const Category = require("../models/category");
 
 const asyncHandler = require("express-async-handler");
 
@@ -8,5 +9,5 @@ const { body, validationResult } = require("express-validator");
 exports.item_list = asyncHandler(async (req, res, next) => {
   const allItems = await Item.find().populate("category").sort({name:1}).exec();
   console.log(allItems);
-  res.render("item_list", { title: "Item List" , item_list: allItems}, );
+  res.render("item_list", { title: "Item List" , items: allItems}, );
 });
