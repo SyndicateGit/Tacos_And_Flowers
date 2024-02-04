a24159ae6b1573b554ba5bd119a97fd3136da055
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-04 13:52:20 -0500

    Added categories page with router

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,7 +4,14 @@ const asyncHandler = require("express-async-handler");
 
 const { body, validationResult } = require("express-validator");
 
+
 //TODO: Implement the category_list controller method
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category List");
+  const allCategory = await Category.find().sort({ name: 1 }).exec();
+	res.render("category_list", {
+		title: "Category List",
+		category_list: allCategory,
+	});
 });
+
+//TODO: Implement the category_create_get controller method
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -13,12 +13,25 @@ exports.item_list = asyncHandler(async (req, res, next) => {
   res.render("item_list", { title: "Item List" , items: allItems}, );
 });
 
-//TODO add item_create_get controller
-
 exports.item_create_get = asyncHandler(async (req, res, next) => {
   const categories = await Category.find().exec();
   res.render("item_form", {
     title: "Create Item",
     categories: categories,
   });
+});
+
+//TODO add item_create_post controller
+exports.item_create_post = asyncHandler(async (req, res, next) => {
+  res.render("Not Implemented: Item Create Post");
+});
+
+//TODO add item_delete_get controller
+exports.item_delete_get = asyncHandler(async (req, res, next) => {
+  res.render("Not Implemented: Item Delete Get");
+});
+
+//TODO add item_delete_post controller
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+  res.render("Not Implemented: Item Delete Post");
 });
