881eb1ff2bf9707f8101899d88091e028c1a0c3b
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-06 10:01:49 -0500

    Added item update functionality
    Learning experience: need to keep _id the same
    when updating an item. Otherwise, it will create a
    new item with a new _id.
    
    Delete cloudinary images when updating an item
    if the old item has an image.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -128,6 +128,102 @@ exports.item_delete_post = asyncHandler(async (req, res, next) => {
   res.redirect("/inventory/items");
 });
 
-//TODO: Handle item update on GET.
+// Handle Item update on GET.
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate("category").exec();
+  const categories = await Category.find().exec();
+  if (item == null) {
+    const err = new Error("Item not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("item_form", {
+    title: "Update Item",
+    item: item,
+    categories: categories,
+  });
+});
 
 //TODO: Handle item update on POST.
+exports.item_update_post = [
+  body("name", "Item name of min 3 characters required")
+  .trim()
+  .isLength({ min: 3 })
+  .escape(),
+  body("description", "Item description of min 3 characters required")
+  .trim()
+  .isLength({ min: 3 })
+  .escape(),
+  body("price", "Price is required")
+  .trim()
+  .isNumeric()
+  .escape(),
+  body("category", "Category is required")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+  
+  asyncHandler(async (req, res, next) => {
+    try{
+      const errors = validationResult(req);
+
+    let formattedPrice = parseFloat(req.body.price).toFixed(2);
+
+    let item;
+    
+    // Image file is uploaded
+    if(req.file){
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      item = new Item({
+        name: req.body.name,
+        category: req.body.category,
+        profile_img: result.secure_url,
+        cloudinary_id: result.public_id,
+        price: formattedPrice,
+        description: req.body.description,
+        _id: req.params.id,
+      });
+    } 
+
+    // Image file is not uploaded
+    else {
+      item = new Item({
+        name: req.body.name,
+        category: req.body.category,
+        price: formattedPrice,
+        description: req.body.description,
+        _id: req.params.id,
+      });
+    }
+
+    // If there are errors, render the form again, passing the previously entered values and errors
+    if (!errors.isEmpty()) {
+      const categories = await Category.find().exec();
+      res.render("item_form", {
+        title: "Create Item",
+        item: item,
+        categories: categories,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+
+      //Fetch old item from db before update.
+      const currItem = await Item.findByIdAndUpdate(req.params.id).exec();
+
+      //Destroy old image in cloudinary if item has img.
+      if(currItem && currItem.cloudinary_id){
+        await cloudinary.uploader.destroy(currItem.cloudinary_id);
+        console.log("Destroyed old image in cloudinary");
+      }
+
+      //Update item in db.
+      await Item.findByIdAndUpdate(req.params.id, item).exec();
+      res.redirect(item.url);
+    }
+  } catch (err) {
+    console.log(err);
+    }
+  }),
+]
--- routes/inventory.js
+++ routes/inventory.js
@@ -11,13 +11,18 @@ const cloudinary = require("../configs/cloudinaryConfig");
 
 /// ITEM ROUTES ///
 
+
+// GET Item pages.
 router.get("/items", itemController.item_list);
-router.post("/items/create", upload.single("uploaded_file"), itemController.item_create_post);
 router.get("/items/create", itemController.item_create_get);
 router.get("/items/:id", itemController.item_detail);
 router.get("/items/:id/delete", itemController.item_delete_get);
-router.post("/items/:id/delete", itemController.item_delete_post);
+router.get("/items/:id/update", itemController.item_update_get);
 
+// POST Item pages.
+router.post("/items/create", upload.single("uploaded_file"), itemController.item_create_post);
+router.post("/items/:id/delete", itemController.item_delete_post);
+router.post("/items/:id/update", upload.single("uploaded_file"), itemController.item_update_post);
 
 // GET categories page.
 router.get("/categories", categoryController.category_list);
@@ -30,6 +35,7 @@ router.get("/categories/:id/update", categoryController.category_update_get);
 router.post("/categories/create", categoryController.category_create_post);
 router.post("/categories/:id/delete", categoryController.category_delete_post);
 router.post("/categories/:id/update", categoryController.category_update_post);
+
 // GET checkout page.
 router.get("/checkout", checkoutController.checkout);
 
