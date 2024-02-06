315cfd99a6d0771cd7866891ba2be963f5584eaa
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-06 14:17:14 -0500

    Changed host ip to 0.0.0.0 and set up fly.io

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -5,6 +5,7 @@ const multer = require("multer");
  * Temporarily stores the uploaded files on the server's disk. 
  * The public/uploads directory is the destination where these files are stored temporarily before being processed & moved to Cloudinary.
  */
+
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, "temp");  
