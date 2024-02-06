7997a7132415dd26de97cf3fedd7877299df7c24
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-06 10:14:26 -0500

    Added checkout page :)

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -7,5 +7,5 @@ const { body, validationResult } = require("express-validator");
 //TODO: Implement checkout page.
 //Am thinking of rick roll for now lol
 exports.checkout = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category List");
+  res.render("checkout", { title: "Checkout" });
 });
